4a96e48ccdea226f5de6783a4cba35a0bb0e4f19
Pablo Estevez <pestevez@fb.com>
2017-12-18 15:36:03 -0500

    Load rules from exported file

--- src/js/components/__mocks__/fs.js
+++ src/js/components/__mocks__/fs.js
@@ -9,15 +9,27 @@
 // Get a mock of the fs module
 const fs = jest.genMockFromModule('fs');
 
+// Variable to store a callback for the readFile function
+let readFileCallback = null;
+// Special function for our tests to set a readFile callback
+fs.__setReadFileCallback = callback => (readFileCallback = callback);
+
+// Override the readFile function with one that calls our callback
+fs.readFile = (filePaths, encoding, processingCallback) => {
+  if (readFileCallback) {
+    readFileCallback(filePaths, encoding, processingCallback);
+  }
+};
+
 // Variable to store a callback for the writeFile function
 let writeFileCallback = null;
 // Special function for our tests to set a writeFile callback
 fs.__setWriteFileCallback = callback => (writeFileCallback = callback);
 
 // Override the writeFile function with one that calls our callback
-fs.writeFile = (fileName, contents, errorCallback) => {
+fs.writeFile = (fileName, contents, encoding, errorCallback) => {
   if (writeFileCallback) {
-    writeFileCallback(fileName, contents, errorCallback);
+    writeFileCallback(fileName, contents, encoding, errorCallback);
   }
 };
 
