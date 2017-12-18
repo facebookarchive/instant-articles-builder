95aa68faea722f28b6d7631977c67625c32f7f2c
Pablo Estevez <pestevez@fb.com>
2017-12-18 15:51:37 -0500

    Update default exported rules test

--- src/js/components/__tests__/RuleList.react-test.js
+++ src/js/components/__tests__/RuleList.react-test.js
@@ -16,23 +16,34 @@ const { mount } = Enzyme;
 const React = require('react');
 const RuleList = require('../RuleList.react.js');
 
+// Rules that are always included in the exported file
+const defaultExportedRules = [
+  {
+    class: 'TextNodeRule',
+  },
+  {
+    class: 'PassThroughRule',
+    selector: '*',
+  },
+];
+
 // Initialize Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('RuleList', () => {
-  it('should export empty rules', () => {
+  it('should export only default rules', () => {
     // Set this function as the one that will be called as fs.writeFile
     fs.__setWriteFileCallback((fileName, contents, errorCallback) => {
-      // Expected exported object has no rules
+      // Expected exported object has onle the default rules
       const expectedRules = {
-        rules: [],
+        rules: [...defaultExportedRules],
       };
       expect(JSON.parse(contents)).toEqual(expectedRules);
     });
 
     // Mount the App component and trigger a click on the Export button
     // Exptected to trigger our writeFile callback
-    mount(<RuleList rules={[]} />)
+    mount(<RuleList onRulesJSONChanged={() => {}} rules={[]} />)
       .find('#export-button')
       .simulate('click');
   });
